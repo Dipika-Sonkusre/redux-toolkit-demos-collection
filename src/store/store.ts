54a1e6b962d11c9948ff72519f116eb887173de4
Dipika-Sonkusre <dipika@weetechsolution.com>
2025-09-06 19:19:54 +0530

    feat: Complete Todo App

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { counterReducer } from "../slice/counterSlice";
+import { counterReducer } from "../slice/Counter/counterSlice";
+import { todoReducer } from "../slice/Todo/todoSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    todo: todoReducer,
   },
 });
 
